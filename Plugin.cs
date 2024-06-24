7211fd47a1f6642d170aaa39adca1231cf70684a
LeShaDDoW <leshaddow3@gmail.com>
2024-06-24 15:52:36 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using WobblyManhunt.ChatLog;
+
+namespace WobblyManhunt
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static AssetBundle bundle { get; private set; }
+
+        private void Awake()
+        {
+            bundle = Utils.QuickLoadAssetBundle("lstwo.wobblymanhunt");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void Start()
+        {
+            ChatLogManager.Init();
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.F10))
+            {
+                ChatLogManager.SendLogMessage("hello");
+            }
+        }
+    }
+
+    public enum PlayerType
+    {
+        Hunter,
+        Player,
+        Spectator
+    }
+
+    public class ManhuntGame
+    {
+        public bool hasStarted = false;
+        public Dictionary<PlayerController, PlayerType> playerTypes = new Dictionary<PlayerController, PlayerType>();
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WobblyManhunt
+{
+    public static class Utils
+    {
+        public static AssetBundle QuickLoadAssetBundle(string assetBundleName)
+        {
+            string text = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), assetBundleName);
+            return AssetBundle.LoadFromFile(text);
+        }
+    }
+}
